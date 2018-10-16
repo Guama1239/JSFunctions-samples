b20a0dd7c2554f3cb817bd6475b1aa4270b209a8
Guama1239 <guama1239@gmail.com>
2018-10-16 15:01:04 -0500

    10/16/2018 my solution and top solution

--- script.js
+++ script.js
@@ -75,7 +75,7 @@
 //   :a / b);
 // }
 
-// solution from kata ( i think my SOLUTION  is the best jijiji ON LIE 69)
+// solution from kata ( i think my SOLUTION is a better option ON LInE 69)
 // function arithmetic(a, b, operator) {
 //   switch (operator) {
 //     case 'add':
@@ -94,6 +94,16 @@
 // console.log(arithmetic(5, 2, "multiply"));
 // console.log(arithmetic(8, 2, "divide"));
 
+// Description: solution found 10/16/2018
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+//     Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// Courtesy of ProjectEuler.net
+
+
 function solution(number) {
     var m3 = []; var m5 = []; var sum = 0;
     for (let index = 0; index < number; index++) {
@@ -104,20 +114,17 @@ function solution(number) {
             m5[index] = 5 * (index + 1);
         }
     }
-    if (m3.length > m5.length) {
-        for (let index = 0; index < m3.length; index++) {
+    for (let index = 0; index < m3.length; index++)
+        {
             if (!isNaN(m5[index]) && (!isNaN(m3[index]))) {
-                if (m3.includes(m5[index])) { sum += m3[index]; }
-                else { sum += m3[index] + m5[index]; }
-            }
-            else { sum += m3[index]; }
-
+                if (m3.includes(m5[index])) { sum += m3[index];
+                }else { sum += m3[index] + m5[index]; }
+            }else { sum += m3[index]; }
         }
-    }
     return sum;
 }
 
-console.log(solution(10));
+console.log(solution(6));
 // function test(n, expected) {
 //   let actual = solution(n)
 //   Test.assertEquals(actual, expected, `Expected ${expected}, got ${actual}`)
