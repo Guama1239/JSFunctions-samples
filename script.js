3729d4f5e3da4256bdf262b471106a7cb9b483db
Guama1239 <guama1239@gmail.com>
2018-12-13 15:40:33 -0600

    gap in primes solution

--- script.js
+++ script.js
@@ -197,3 +197,174 @@
 // console.log(humanYearsCatYearsDogYears(2));
 // console.log(humanYearsCatYearsDogYears(10));
 
+// lines 200 to 214 work along file index2.html
+// var myTarget = document.querySelector('#myLinks');
+// var arr = [
+//     { name: "LinkedIn", link: "http://linkedin.com/in/planetoftheweb" },
+//     { name: "Blog", link: "http://raybo.org" },
+//     { name: "Twitter", link: "http://twitter.com/planetoftheweb" }
+// ];
+
+// arr.forEach(function (item) {
+//     var el = document.createElement('a');
+//     el.href = item.link;
+//     el.innerText = item.name;
+
+//     myTarget.appendChild(el);
+// });
+
+// 11/30/2018
+// The prime numbers are not regularly spaced.For example from 2 to 3 the gap is 1. From 3 to 5 the gap is 2. From 7 to 11 it is 4. Between 2 and 50 we have the following pairs of 2 - gaps primes: 3 - 5, 5 - 7, 11 - 13, 17 - 19, 29 - 31, 41 - 43
+
+// A prime gap of length n is a run of n - 1 consecutive composite numbers between two successive primes(see: http://mathworld.wolfram.com/PrimeGaps.html).
+
+//     We will write a function gap with parameters:
+
+//     g(integer >= 2) which indicates the gap we are looking for
+
+// m(integer > 2) which gives the start of the search(m inclusive)
+
+// n(integer >= m) which gives the end of the search(n inclusive)
+
+// In the example above gap(2, 3, 50) will return [3, 5] or(3, 5) or { 3, 5 } which is the first pair between 3 and 50 with a 2 - gap.
+
+// So this function should return the first pair of two prime numbers spaced with a gap of g between the limits m, n if these numbers exist otherwise nil or null or None or Nothing(depending on the language).
+
+// In C++ return in such a case { 0, 0 }.In F# return [||].In Kotlin return []
+
+// #Examples: gap(2, 5, 7)-- > [5, 7] or(5, 7) or { 5, 7 }
+
+// gap(2, 5, 5)-- > nil.In C++ { 0, 0 }.In F#[||].In Kotlin return []`
+
+// gap(4, 130, 200) --> [163, 167] or (163, 167) or {163, 167}
+
+// ([193, 197] is also such a 4-gap primes between 130 and 200 but it's not the first pair)
+
+// gap(6,100,110) --> nil or {0, 0} : between 100 and 110 we have 101, 103, 107, 109 but 101-107is not a 6-gap because there is 103in between and 103-109is not a 6-gap because there is 107in between.
+
+// Note for Go
+// For Go: nil slice is expected when there are no gap between m and n. Example: gap(11,30000,100000) --> nil
+
+// #Ref https://en.wikipedia.org/wiki/Prime_gap
+
+//less optimize without the use of Math.sqrt(n)
+        // function gap(g, m, n) {
+        //     let primeNums = [];
+            
+        //     for (let index = m; index <= n; index++) {
+        //         isPrime = true;
+        //         if (index === 2) {primeNums.push(index)} else {
+        //         for (let i = 3; i <index; i++, i++) {
+        //             if (index % i === 0) {isPrime = false};
+        //         }}   
+        //         if (index !==2 && isPrime) {primeNums.push(index)};
+        //     }
+        //     for (let i = 0; i <= primeNums.length; i++) {
+        //         if (primeNums[i] + g === primeNums[i + 1]) {
+        //             return [primeNums[i], primeNums[(i + 1)]];
+        //         } else if (i === primeNums.length) {
+        //             return null;
+        //         }
+        //     };
+        // };
+
+        // function gap(g, m, n) {
+        //     let primeNums = [];
+            
+        //     for (let index = m; index <= n; index++) {
+        //         isPrime = true;
+        //         if (index === 2) {primeNums.push(index)} else {
+        //         for (let i = 3; i <= Math.sqrt(index); i++, i++) {
+        //             if (index % i === 0 || index % 2 === 0) {isPrime = false};
+        //         }}   
+        //         if (index !==2 && isPrime) {primeNums.push(index)};
+        //     }
+        //     for (let i = 0; i <= primeNums.length; i++) {
+        //         if (primeNums[i] + g === primeNums[i + 1]) {
+        //             return [primeNums[i], primeNums[(i + 1)]];
+        //         } else if (i === primeNums.length) {
+        //             return null;
+        //         }
+        //     };
+        // };
+// the solution below starting from line 291 to 311 optimizes it by jumping m plus 1 (m++) disregarding even numbers execept number 2.
+        // function gap(g, m, n) {
+        //     let primeNums = [];
+        //     if (m === 2) { primeNums.push(m) };
+        //     if ( m % 2 === 0) {m++};
+
+        //     for (let index = m; index <= n; index++,index++) {
+        //         isPrime = true;
+        //          if (index === 2) {primeNums.push(index)} else {
+        //         for (let i = 3; i <= Math.sqrt(index); i++, i++) {
+        //             if (index % i === 0 || index % 2 === 0) {isPrime = false};
+        //         }}
+        //         if (index !==2 && isPrime) {primeNums.push(index)};
+        //     }
+        //     for (let i = 0; i <= primeNums.length; i++) {
+        //         if (primeNums[i] + g === primeNums[i + 1]) {
+        //             return [primeNums[i], primeNums[(i + 1)]];
+        //         } else if (i === primeNums.length) {
+        //             return null;
+        //         }
+        //     };
+        // };
+
+// great solution passed all tests 12/13/2018
+function gap(g, m, n) {
+            let primeNums = [];
+            if (m === 2) { primeNums.push(m) };
+            if ( m % 2 === 0) {m++};
+
+            for (let index = m; index <= n+10; index++,index++) {
+                isPrime = true;
+                 if (index === 2) {primeNums.push(index)} else {
+                for (let i = 3; i <= Math.sqrt(index); i++, i++) {
+                    if (index % i === 0 || index % 2 === 0) {isPrime = false};
+                }}
+                if (index !==2 && isPrime) {primeNums.push(index)};
+                if (primeNums.length>2){
+                    for (let i = 1; i < primeNums.length; i++) {
+                        if (primeNums[i-1] + g === primeNums[i]) {
+                            return [primeNums[i-1], primeNums[i]];
+                        } else if (primeNums[i] >= n) {
+                                return null;
+                        }
+                    }
+                }
+            }
+            
+        };
+
+        
+
+console.log(gap(1,2,50));
+console.log(gap(2,100,110));
+console.log(gap(4,100,110));
+console.log(gap(6,100,110));
+console.log(gap(8, 300, 400));
+console.log(gap(10, 300, 400));
+console.log(gap(4, 30100, 30120));
+console.log(gap(6, 30000, 30100));
+console.log(gap(8, 30100, 30200));
+
+
+// gap(0,2,0);
+// gap(0,3,0);
+// gap(0, 5, 0);
+// gap(0, 359, 0);
+// gap(0, 7, 0);
+// function sortArray(array){
+//    if (array !== []){
+//        let odd = array.filter(function(v){return v % 2 ==1;});
+//        odd.sort(function(a,b){return a-b});
+//        for (let index = 0; index < array.length; index++) {
+//            if (array[index] % 2 == 0){
+//             odd.splice(index, 0, array[index]);
+//            };
+
+//        }
+//        return odd;
+//    }
+//    return ('[]');
+// };
